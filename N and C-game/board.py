70af015fd97d7e4889c9ccea20cb12a97e66f103
SandorDiGriz <al_potechin@mail.ru>
2021-08-22 00:08:46 +0300

    Input tests added, docs and comments added
    Numerous changes in catching bugs, several functions have been added: checking the player's input, determining the player's move, the code is documented and commented out

--- N and C-game/board.py
+++ N and C-game/board.py
@@ -1,14 +1,35 @@
+'''File that defines the "Board" class and all operations with the grid
+'''
+
 from prettytable import PrettyTable
 import string
 
 
 class Board:
+    '''Class implements all functions for creating the playing
+    field, changing it and checking for a win or draw
+    '''
 
-    def __init__(self, row_names=[''], rows=[]):
+    def __init__(self, row_names=None, rows=None):
+        '''Initialisation of grid's markup'''
+        if row_names is None:
+            # Adding an empty string for sync of columns and lines
+            row_names = ['']
+        if rows is None:
+            rows = []
         self.row_names = row_names 
         self.rows = rows
 
-    def building_grid(self, height, width):
+
+    def build_grid(self, width, height):
+        '''Building the grid using input's data
+        
+        Args:
+        width(str): width of the grid
+        height(str): height of the grid
+        
+        '''
+        # String method is required for column names
         count = 0
         for i in string.ascii_uppercase:
             if count < width:
@@ -16,25 +37,35 @@ class Board:
                 count += 1
 
         for j in range(height):
-            row = ['' for _ in range(width + 1)]
-            row[0] = j + 1
+            # Filling the grid with empty strings
+            row = ['' for _ in range(width)]
+            row.insert(0, j + 1)
             self.rows.append(row)
-        return self
+        return
+
 
     def get_diag(self):
+        '''Gets all diagonals in current grid'''
         diags = []
+        # Size of column is equal to the rows in the grid
         column_size = len(self.rows)
+        # Size of the line minus its number
         line_size = len(self.rows[0]) - 1
+        # Calculatimg the diagonals of the symmetric or asymmetric grid
         if line_size == column_size:
+            # Diags in both directions
             right_diag = []
             left_diag = []
+            # Counter does not allow you to go outside the grid
             diag_count = 1
             for row in self.rows:
                 if diag_count == line_size + 1:
                     break
+                # Addendum of diags from opposite sides
                 right_diag.append(row[diag_count])
                 left_diag.append(row[-diag_count])
                 diag_count += 1
+            # Getting the matrix with all diags
             diags = [right_diag] + [left_diag]
         elif line_size < column_size:
             for diag in range(column_size):
@@ -43,7 +74,9 @@ class Board:
                 right_diag = []
                 left_diag = []
                 diag_count = 1
+                # Additional counter for enumeration of lines
                 row = diag
+                # Cycle is a counter for the element's line position
                 for _ in range(line_size):
                     right_diag.append(self.rows[row][diag_count])
                     left_diag.append(self.rows[row][-diag_count])
@@ -54,6 +87,7 @@ class Board:
         else:
             diag_count, line_count = 1, 1
             for diag in range(line_size):
+                # Resetting diag_count 
                 diag_count = line_count
                 if line_size - diag < column_size:
                     break
@@ -70,11 +104,20 @@ class Board:
                 diags.append(left_diag)
         return diags
 
+
     def check_diag(self, diags, letter, win_row_size):
-        for i in diags:
+        '''Checks diagonals for wincondition
+        
+        Args:
+        diags (list): all diagonals in the grid
+        letter (str): "X" or "O"
+        win_row_size (int): number of consecutive characters for a win
+
+        '''
+        for diag in diags:
             win_diag_count = 0
-            for j in i:
-                if j == letter:
+            for elem in diag:
+                if elem == letter:
                     win_diag_count += 1
                 else:
                     win_diag_count = 0
@@ -82,10 +125,22 @@ class Board:
                     return True     
         return False
 
+
     def check_vertical_and_flat(self, column, letter, win_row_size):
+        '''Checks vertical and horisontal axes for a wincondition
+        
+        Args:
+        column (int): number of a column
+        letter (str): "X" or "O"
+        win_row_size (int): number of consecutive characters for a win
+        
+        '''
         win_column_count = 0
+        win_line_count = 0
         for row in self.rows:
             for elem in row:
+                # Line examination
+                # Checking symbol for matching the target
                 if elem == letter:
                     win_line_count += 1
                 else:
@@ -93,6 +148,8 @@ class Board:
                 if win_line_count == win_row_size:
                     return True 
             win_line_count = 0
+            # Column examination
+            # Checking symbol for matching the target
             if row[column] == letter:
                 win_column_count += 1
             else:
@@ -101,7 +158,17 @@ class Board:
                 return True
         return False
 
+
     def check_for_winner(self, column, line, turn, win_row_size):
+        '''Checks for a winconditions in all directions
+        
+        Args:
+        column (int): number of a column
+        line (int): number of a line
+        letter (str): "X" or "O"
+        win_row_size (int): number of consecutive characters for a win
+        
+        '''
         column = string.ascii_uppercase.index(column) + 1
         letter = self.define_turn(turn)
         victory = False
@@ -113,39 +180,114 @@ class Board:
         return victory
 
 
-    
-    def game_over(victory):
-        if victory:
-            return True
+    def check_for_draw(self):
+        '''Checks grid to be totally filled'''
+        for row in self.rows:
+            for elem in row:
+                if elem == '':
+                    return False
+        return True
+
 
     def get_coordinates(self):
-        print('print coordinates')
-        coordinates = input().split()
-        coordinates[1] = int(coordinates[1]) - 1
-        return coordinates
+        '''Gets coordinates of player's move'''
+        # Catching possible input errors
+        checking = True
+        while checking:
+            print('input coordinates to make your move')
+            coordinates = input().split()
+            x_coordinate = coordinates[0]
+            y_coordinate = coordinates[1]
+            if x_coordinate == y_coordinate:
+                print('Incorrect letter of a column or number of a line')
+                continue
+            if self.check_coordinates(x_coordinate, y_coordinate):
+                y_coordinate = int(y_coordinate) - 1
+                checking = not checking
+        return x_coordinate, y_coordinate
+
+    def check_coordinates(self, x, y):
+        '''Checks for errors in player's input
+        
+        Args:
+        x (str): letter of a column
+        y (str): number of a line
+        
+        '''
+        # Element could be already converted in int 
+        if type(y) == int:
+            pass
+        # Checking whether the element is a digit
+        elif y.isdigit() == False:
+            print('Incorrect letter of a column or number of a line')
+            return False
+        # Checking whether the element is letter in the grid
+        if x not in self.row_names:
+            print('Incorrect letter of a column or number of a line')
+            return False
+        # Checking borders
+        elif int(y) > len(self.rows) or int(y) < 0:
+            print('Incorrect letter of a column or number of a line')
+            return False
+        return True
+
+
+    def define_player(self, p1, p2, turn):
+        '''Defines whose turn it is
+        
+        Args:
+        p1 (str): name of the first player
+        p2 (str): name of the second player
+        turn(int): number of a turn
+        
+        '''
+        if turn % 2 == 0:
+            return p2
+        else:
+            return p1
 
 
     def define_turn(self, turn):
+        '''Defines character'''
         if turn % 2 == 0:
             return 'O'
         else:
             return 'X'
 
+
     def update_grid(self, column, line, turn):
+        '''Checks for errors in player's input
+        
+        Args:
+        column (str): letter of a column
+        line (int): number of a line
+        turn(int): number of a turn
+        
+        '''
+        # Converting a letter to a number in alphabetical order
         column = string.ascii_uppercase.index(column) + 1
+        # Defining the letter
         letter = self.define_turn(turn)
+        # Adding an element to the grid
         try:
+            # Checking for overlay
             if self.rows[line][column] != '':
                 raise Exception('Overlay exception')
             self.rows[line][column] = letter
+        # Repeating the function if necessary
         except Exception as e:
-            print(e)
+            print('That place is occupied')
             coordinates = self.get_coordinates()
             self.update_grid(str(coordinates[0]), int(coordinates[1]), turn)
 
+
     def print_grid(self):
+        '''Converts and prints the grid in "PrettyTable" view'''
         grid = PrettyTable()
+        # Getting letters for column names
         grid.field_names = self.row_names
+        # Adding the matrix with new data
         grid.add_rows(self.rows)
+        # lign the numbering of lines
         grid.align[''] = 'l'
         return print(grid)
--- N and C-game/play.py
+++ N and C-game/play.py
@@ -1,38 +1,55 @@
+'''Executive file running the game importing 'Board' parameters'''
+
 from board import Board
 
-def define_winner(turn):
-    if turn % 2 == 0:
-        return 
-    else:
-        return 
 
 def play():
+    '''Gets players' data and runs the game'''
     print('Welcome, strangers!', '\nPlease, define your grid (''3 3'' for example)')
-    grid_size = list(map(int, input().split()))
-    height, width = grid_size[0], grid_size[1]
+    print('Remember, that you inputting the width first!')
+    # Importing 'Board' class settings
     board = Board()
-    board.building_grid(width, height)
+    # Setting size of grid
+    grid_size = list(map(int, input().split()))
+    width, height = grid_size[0], grid_size[1]
+    board.build_grid(width, height)
+    board.print_grid()
+    print('You will need to choose a column and a row number to make a move' '\nGood luck!')
+    # Setting turns' counter
     turn = 0
+    # Defining size of a win row
     win_row_size = min(grid_size)
 
-    print('P1, introduce yourself')
+    print('"X" player, introduce yourself')
     player_1 = input()
-    print('P2, introduce yourself')
+    print('"O" player, introduce yourself')
     player_2 = input()
-
     winner = None
+    # Setting game flag
     game = True
     while game:
         turn += 1
         board.print_grid()
-        coordinates = board.get_coordinates()
-        board.update_grid(coordinates[0], coordinates[1], turn)
-        if board.check_for_winner(coordinates[0], coordinates[1], turn, win_row_size):
+        print(board.define_player(player_1, player_2, turn) + ',' '\nYour turn!')
+        # Getting player's move coordinates
+        x_coordinate, y_coordinate = board.get_coordinates()
+        board.update_grid(x_coordinate, y_coordinate, turn)
+        if board.check_for_winner(x_coordinate, y_coordinate, turn, win_row_size):
             board.print_grid()
             winner = player_2 if turn % 2 == 0 else player_1
-            print('GAME OVER' + '\n', winner, 'wins')
+            print('GAME OVER')
+            print(winner, 'wins')
             game = not game
-            
+        elif board.check_for_draw():
+            board.print_grid()
+            print('GAME OVER')
+            print('Friendship wins')
+            game = not game
 
 
-play()
+def main():
+    '''Runs the game'''
+    play()
+
+
+main()
